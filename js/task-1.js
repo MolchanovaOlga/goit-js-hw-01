93ad28220f0756f12675500058f4a498c67170c5
MolchanovaOlga <designer.molchanova@gmail.com>
2023-10-19 22:52:26 +0300

    task-2 and task-3 are solved

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,5 @@
-`use strict`
+`use strict`;
+
 function makeTransaction (quantity, pricePerDroid) {
 console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
 }
---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+function getElementWidth (content, padding, border) {
+    console.log(Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border));
+}
+
+console.log(getElementWidth("50px", "8px", "4px"))
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('1px', '1px', '1px'));
